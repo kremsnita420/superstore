4c6072a46bcc3261d62f3d700ab2e7023095215f
kremsnita420 <duranovic.safet@gmail.com>
2021-10-08 12:33:53 +0200

    navbar links

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,31 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import NextLink from 'next/link'
+import useStyles from '../src/styles'
+import { AppBar, Link, Toolbar, Typography } from '@mui/material'
 
 export default function Header() {
+	const classes = useStyles()
+
 	return (
-		<AppBar position='static'>
+		<AppBar position='static' className={classes.navbar}>
 			<Toolbar>
-				<Typography>Superstore</Typography>
+				<NextLink href='/' passHref>
+					<Link>
+						<Typography className={classes.brand}>
+							Superstore
+						</Typography>
+					</Link>
+				</NextLink>
+				{/* push links to left and right with this div */}
+				<div className={classes.grow}></div>
+
+				<div>
+					<NextLink href='/cart' passHref>
+						<Link>Cart</Link>
+					</NextLink>
+					<NextLink href='/login' passHref>
+						<Link>Login</Link>
+					</NextLink>
+				</div>
 			</Toolbar>
 		</AppBar>
 	)
--- src/styles.js
+++ src/styles.js
@@ -3,10 +3,18 @@ import { makeStyles } from '@mui/styles'
 const useStyles = makeStyles({
     navbar: {
         backgroundColor: '#203040',
-        '& p': {
+        '& a': {
             color: '#fff',
             marginLeft: 10,
+            textDecoration: 'none'
         },
+    },
+    brand: {
+        fontWeight: 'bold',
+        fontSize: '1.5rem',
+    },
+    grow: {
+        flexGrow: 1,
     },
     main: {
         minHeight: '85vh',
